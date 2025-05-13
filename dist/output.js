dc11b2e784b61eda40a69cc4b752e8864bf6f297
fiioonnn <fionnres@gmail.com>
2025-05-13 14:48:02 +0200

    build

--- dist/output.js
+++ dist/output.js
@@ -15,14 +15,64 @@ const log = (...args) => {
         return;
     console.log(`[${config.prefix}]`, ...args);
 };
+/** Custom error log prefix, used to make debugging easier. */
+const error = (...args) => {
+    if (!config.debug)
+        return;
+    console.error(`[${config.prefix}]`, ...args);
+};
+/** Returns the device type of the visitor. */
+const getDeviceType = () => {
+    const ua = navigator.userAgent;
+    if (/Tablet|iPad/i.test(ua))
+        return "tablet";
+    if (/Mobile|Android|iPhone|iPod|IEMobile|BlackBerry/i.test(ua))
+        return "mobile";
+    return "desktop";
+};
+/** Returns the referring domain. */
+const getReferringDomain = (referrer) => {
+    if (!referrer)
+        return "";
+    return new URL(referrer).hostname;
+};
+/** Returns the visitors UTM parameters. */
+const getUTMParameters = () => {
+    const params = new URLSearchParams(window.location.search);
+    const keys = [
+        "utm_source",
+        "utm_medium",
+        "utm_campaign",
+        "utm_term",
+        "utm_content",
+    ];
+    return keys.reduce((acc, key) => {
+        const value = params.get(key);
+        if (value)
+            acc[key] = value;
+        return acc;
+    }, {});
+};
+/** Emits an event to the Shopify pixel. */
+const emit = (eventType, eventData = null) => {
+    try {
+        Shopify.analytics.publish(`abone_${eventType}`, eventData !== null && eventData !== void 0 ? eventData : undefined);
+        log({ eventType, eventData });
+    }
+    catch (e) {
+        error("Could not send event:", e);
+    }
+};
 //
 // Main script
 //
 const abone = (tests) => {
-    if (!Shopify) {
-        log("Shopify is not defined");
-        return;
-    }
-    console.log(123, tests);
+    if (Shopify.designMode)
+        return log("Design mode detected. Abandoning script execution.");
+    emit("pageview", {
+        device: getDeviceType(),
+        utmParams: getUTMParameters(),
+        referrer: getReferringDomain(document.referrer),
+    });
     log("Script loaded successfully.");
 };
--- src/main.ts
+++ src/main.ts
@@ -18,15 +18,72 @@ const log = (...args: any[]) => {
 	console.log(`[${config.prefix}]`, ...args);
 };
 
+/** Custom error log prefix, used to make debugging easier. */
+const error = (...args: any[]) => {
+	if (!config.debug) return;
+
+	console.error(`[${config.prefix}]`, ...args);
+};
+
+/** Returns the device type of the visitor. */
+const getDeviceType = (): Device => {
+	const ua = navigator.userAgent;
+
+	if (/Tablet|iPad/i.test(ua)) return "tablet";
+	if (/Mobile|Android|iPhone|iPod|IEMobile|BlackBerry/i.test(ua))
+		return "mobile";
+	return "desktop";
+};
+
+/** Returns the referring domain. */
+const getReferringDomain = (referrer: string): string => {
+	if (!referrer) return "";
+
+	return new URL(referrer).hostname;
+};
+
+/** Returns the visitors UTM parameters. */
+const getUTMParameters = (): Record<string, string> => {
+	const params = new URLSearchParams(window.location.search);
+	const keys = [
+		"utm_source",
+		"utm_medium",
+		"utm_campaign",
+		"utm_term",
+		"utm_content",
+	];
+
+	return keys.reduce((acc, key) => {
+		const value = params.get(key);
+		if (value) acc[key] = value;
+		return acc;
+	}, {} as Record<string, string>);
+};
+
+/** Emits an event to the Shopify pixel. */
+const emit = (eventType: string, eventData: any = null): void => {
+	try {
+		Shopify.analytics.publish(`abone_${eventType}`, eventData ?? undefined);
+
+		log({ eventType, eventData });
+	} catch (e) {
+		error("Could not send event:", e);
+	}
+};
+
 //
 // Main script
 //
 
 const abone = (tests: any) => {
-	if (!Shopify) {
-		log("Shopify is not defined");
-		return;
-	}
-	console.log(123, tests);
+	if (Shopify.designMode)
+		return log("Design mode detected. Abandoning script execution.");
+
+	emit("pageview", {
+		device: getDeviceType(),
+		utmParams: getUTMParameters(),
+		referrer: getReferringDomain(document.referrer),
+	});
+
 	log("Script loaded successfully.");
 };
